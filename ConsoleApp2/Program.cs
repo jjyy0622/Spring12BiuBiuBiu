4ddfc8751c78adadf30f3f1b322f7bdd7d2f0802
Leroy.Haoyu.Liu <lhy20010403@hotmail.com>
2019-02-27 18:37:08 +0800

    添加一些sleep，效果看起来更真实

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -11,13 +11,14 @@ namespace Spring12BiuBiuBiu
     {
         static void Main(string[] args)
         {
+            Console.Title = "春节十二响 - 行星发动机控制程序";
             Console.WriteLine("您好，请输入您的全球唯一识别ID：");
             Console.ReadLine();
             Console.WriteLine("正在获取【苏拉威西三号转向发动机点火权限】授权。。。\n");
             ProgressBar progressBar1 = new ProgressBar(Console.CursorLeft, Console.CursorTop, 50, ProgressBarType.Multicolor);
             for (int Getadmit = 1; Getadmit <= 100; Getadmit++)
             {
-                Thread.Sleep(50);
+                Thread.Sleep(GetRandomSleep());
                 progressBar1.Dispaly(Getadmit);
             }
             Thread.Sleep(100);
@@ -28,14 +29,16 @@ namespace Spring12BiuBiuBiu
             Console.WriteLine("请在等候时阅读【李一一】工程师编写的《春节十二响操作手册》。\n");
 
             Thread.Sleep(1000);
-            Console.WriteLine("硬件系统自检完成...\n");
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("[OK] 硬件系统自检完成...\n");
             Thread.Sleep(1000);
-            Console.WriteLine("撞针系统自检完成...\n");
+            Console.WriteLine("[OK] 撞针系统自检完成...\n");
             Thread.Sleep(1000);
-            Console.WriteLine("行星转向发动机正在启动中！启动进度：\n");
+            Console.WriteLine("[OK] 行星转向发动机正在启动中！启动进度：\n");
             progressBar1 = new ProgressBar(Console.CursorLeft, Console.CursorTop, 50, ProgressBarType.Character);
-            for (int test = 1; test <= 100; test++) {
-                Thread.Sleep(50);
+            for (int test = 1; test <= 100; test++)
+            {
+                Thread.Sleep(GetRandomSleep());
                 progressBar1.Dispaly(test);
             }
             Console.WriteLine("\n");
@@ -43,6 +46,7 @@ namespace Spring12BiuBiuBiu
             Console.WriteLine("行星转向发动机已启动");
             Console.WriteLine("\n");
             Thread.Sleep(1000);
+            Console.ResetColor();
             int i;
             for (i = 1; i <= 12; i++)
             {
@@ -58,7 +62,7 @@ namespace Spring12BiuBiuBiu
             Console.WriteLine("\n");
             while (a >= 1)
             {
-                
+
                 Console.WriteLine("                        Biu!!!\n");
                 a -= 1;
                 Thread.Sleep(100);
@@ -87,6 +91,12 @@ namespace Spring12BiuBiuBiu
             Thread.Sleep(1000);
             Console.ReadKey();
         }
+
+        private static int GetRandomSleep()
+        {
+            return new Random().Next(50, 400);
+        }
+
         static void fire()
         {
             Console.ForegroundColor = ConsoleColor.Green;
@@ -101,6 +111,6 @@ namespace Spring12BiuBiuBiu
             Console.Clear();//必须
             Console.WriteLine("Biu~");
         }
-        
+
     }
 }
